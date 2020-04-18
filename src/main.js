eb0ece3b7c4df4aa748d517dc65daa94facf8136
Rodolpho Netto <rodolpho.netto@gmail.com>
2020-04-18 17:01:10 -0300

    instalado sistema de layouts

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import { Container, Input, Row, Col, Button } from "element-ui"
 
-Vue.config.productionTip = false;
+import "element-ui/lib/theme-chalk/index.css"
+
+Vue.component(Input.name, Input)
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.component(Button.name, Button)
+Vue.component(Container.name, Container)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount("#app")
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="login">
+    <el-row
+      type="flex"
+      justify="center"
+      align="middle"
+      style="height: 100vh; margin-bottom: 0"
+    >
+      <el-col :span="4">
+        <el-container direction="vertical">
+          <el-row>
+            <el-col>
+              <p>Por favor entre com suas credenciais</p>
+            </el-col>
+          </el-row>
+          <el-row type="flex" justify="center">
+            <el-col :span="24">
+              <el-input placeholder="E-mail" v-model="input" clearable>
+              </el-input>
+            </el-col>
+          </el-row>
+          <el-row type="flex" justify="center">
+            <el-col :span="24">
+              <el-input placeholder="E-mail" v-model="input" clearable>
+              </el-input>
+            </el-col>
+          </el-row>
+          <el-row type="flex" justify="end">
+            <el-col class="col-end" :span="8">
+              <el-button type="success">Login</el-button>
+            </el-col>
+          </el-row>
+        </el-container>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+    }
+  },
+  name: "Login",
+  components: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+
+.col-end {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
